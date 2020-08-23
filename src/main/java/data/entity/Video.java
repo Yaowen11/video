d5a28e344c46da5a275cedb48412d7c27ec86f18
zhangyaowen <760735372@qq.com>
2020-08-23 13:49:21 +0800

    init project

---
+++ src/main/java/data/entity/Video.java
@@ -0,0 +1,8 @@
+package data.entity;
+
+/**
+ * @author zyw
+ * @date 2020/8/22 11:07
+ */
+public class Video {
+}
---
+++ src/main/java/data/repository/VideoRepository.java
@@ -0,0 +1,8 @@
+package data.repository;
+
+/**
+ * @author zyw
+ * @date 2020/8/22 11:07
+ */
+public class   VideoRepository {
+}
